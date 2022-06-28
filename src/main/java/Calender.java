869a7926d89e8a3b14319d8553cd00add9ab0f51
shivam <shivamguptaniki@gmail.com>
2022-06-29 00:34:08 +0530

    Test Regarding calender class is added

--- src/main/java/Calender.java
+++ src/main/java/Calender.java
@@ -15,3 +15,8 @@ public class Calender
         return isAvailable;
     }
 }
+
+
+// 1. person whole slots is empty then slots are available return true
+// 2. person slot overlap with start and end then return false
+// 3. person slot does not overlap with start and end then return true
